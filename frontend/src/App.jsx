9afbba7c88bd8a84872a7f1b62bc3263d9067b55
HashenRubix <hashenpersonal@gmail.com>
2026-09-23 16:15:11 +0530

    feat: add login page with form state

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import CreatePost from "./pages/CreatePost";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/create" element={<CreatePost />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
